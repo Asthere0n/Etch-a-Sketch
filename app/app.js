9be3afefa2a53599a28f8a95070600b0ea55381c
Asthere0n <cristiansanchezsonido@gmail.com>
2024-10-08 00:13:27 +0100

    Correcting Bug: Changing the text imput didn't adjust the grid correctly. SOLVED!

--- app/app.js
+++ app/app.js
@@ -10,12 +10,17 @@ resetPixelScreen = function () {
 //Creando los pixeles de nuestro Canvas
 createPixelScreen = function (side) {
     resetPixelScreen()
+
     for (i = 0; i < side * side; i++) {
         const div = document.createElement('div')
         div.classList.add('pixel')
         screen.appendChild(div)
     }
+    console.log(side*side + ' pixeles creados')
 
+    
+
+    //Pintando en la pantalla
     const screenPixels = document.querySelectorAll('.pixel')
 
     screenPixels.forEach((pixel) => {
@@ -25,11 +30,13 @@ createPixelScreen = function (side) {
     });
 }
 
-const pixelResolution = document.querySelector('#resolution').value
 
-document.querySelector('#createButton').addEventListener('click', () => { createPixelScreen(pixelResolution) })
+document.querySelector('#createButton').addEventListener('click', () => {
+    const pixelResolution = document.querySelector('#resolution').value
+    createPixelScreen(pixelResolution)
+})
 
 
 
-//Pintando en la pantalla
+
 

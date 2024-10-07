8a72afd87a7eb7d450f6d356ea363c5719c04358
Asthere0n <cristiansanchezsonido@gmail.com>
2024-10-07 23:35:40 +0100

    Project basic requirements finished

--- app/app.js
+++ app/app.js
@@ -1,1 +1,35 @@
-console.log('Hello Git')
+const screen = document.querySelector('.screen')
+
+//Reseteando la pantalla
+resetPixelScreen = function () {
+    while (screen.firstChild) {
+        screen.removeChild(screen.firstChild)
+    }
+}
+
+//Creando los pixeles de nuestro Canvas
+createPixelScreen = function (side) {
+    resetPixelScreen()
+    for (i = 0; i < side * side; i++) {
+        const div = document.createElement('div')
+        div.classList.add('pixel')
+        screen.appendChild(div)
+    }
+
+    const screenPixels = document.querySelectorAll('.pixel')
+
+    screenPixels.forEach((pixel) => {
+        pixel.addEventListener('mouseenter', () => {
+            pixel.classList.add('painted')
+        })
+    });
+}
+
+const pixelResolution = document.querySelector('#resolution').value
+
+document.querySelector('#createButton').addEventListener('click', () => { createPixelScreen(pixelResolution) })
+
+
+
+//Pintando en la pantalla
+
